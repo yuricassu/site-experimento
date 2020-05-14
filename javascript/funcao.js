a33880f229712d9063a85aa4ae3d66da813998be
yuricassu <vcassu@gmail.com>
2020-05-14 03:33:31 -0300

    mudanças

---
+++ javascript/funcao.js
@@ -0,0 +1,23 @@
+function mudafoto(foto){
+    document.querySelector("img#icone").src=foto;
+      }
+      function cliques1(){
+        
+          window.open('https://i.pinimg.com/originals/ab/51/23/ab5123d181e7682cd214bbd640c14cc1.jpg');
+      }
+       function cliques2(){
+        window.open('https://i.pinimg.com/originals/37/2b/0f/372b0f2edfc0af0494f2a25d8b106e1e.jpg');
+       }
+       function cliques3(){
+         window.open('https://i.pinimg.com/originals/e2/50/05/e250051341f4410e22e6b5ee3d9f2499.jpg')
+       }
+       function cliques4(){
+        window.open('https://i.pinimg.com/originals/ab/0b/9a/ab0b9a21c731a17318afbd64294d6403.jpg')
+      }
+      function cliques5(){
+        window.open('https://i.pinimg.com/originals/e5/c1/0f/e5c10f651e3d7d6fc94936aee66f8581.jpg')
+      }
+      function cliques6(){
+        window.open('https://i.pinimg.com/originals/ca/37/c4/ca37c4a5ea0a4cca461f6b63dac8005d.jpg')
+      }
+    
